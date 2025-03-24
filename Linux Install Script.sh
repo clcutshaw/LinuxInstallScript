879501aec545049627b4903f0ac175611962474a
clcutshaw <99746483+clcutshaw@users.noreply.github.com>
2025-03-24 14:45:04 -0600

    Add files via upload

---
+++ Linux Install Script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Welcome Colton. Beginning installation of your Linux Configuration"
+
+echo "Current Working Directory is"
+pwd
+
+echo "Installation complete. Media will now self delete. Have a good day."
+sudo shutdown -r now
