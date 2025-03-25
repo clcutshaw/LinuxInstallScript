6d8e3abe401e5ff0bd2c890ab609eedb031cd7c7
clcutshaw <99746483+clcutshaw@users.noreply.github.com>
2025-03-25 09:25:19 -0600

    Delete Linux Install Script.sh
    deleted and replaced with better named file

--- Linux Install Script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Welcome Colton. Beginning installation of your Linux Configuration"
-
-echo "Current Working Directory is"
-pwd
-
-echo "Installation complete. Media will now self delete. Have a good day."
-sudo shutdown -r now
