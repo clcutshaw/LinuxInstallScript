5cc27af3f75e685a61e0494686808a3a00aba7e8
clcutshaw <99746483+clcutshaw@users.noreply.github.com>
2025-03-25 09:23:23 -0600

    Add files via upload

---
+++ LinuxInstallScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Welcome Colton. Beginning installation of your Linux Configuration"
+
+echo "Installation complete. Media will now self delete. Have a good day."
+sudo shutdown -r now
