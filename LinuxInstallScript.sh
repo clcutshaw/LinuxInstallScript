5da925c5d15567cd249f35cb011c93411f3c99e7
clcutshaw <99746483+clcutshaw@users.noreply.github.com>
2025-04-30 12:01:35 -0600

    Update LinuxInstallScript.sh
    added OS Detection from LinuxInstallSriptOSD.ext

--- LinuxInstallScript.sh
+++ LinuxInstallScript.sh
@@ -1,16 +1,46 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 #To be run after adding machine local user to sudo group
 echo "Welcome User. Beginning installation of your Linux Configuration"
-echo “Do you wish to proceed?”
+#echo “Do you wish to proceed?”
 
 sudo apt update
 sudo apt upgrade -y
+
+# This populates the OS Identification Data
+source /etc/os-release
+
+debianinstall () { #Function for KDE install on Debian
 sudo apt install xorg -y
 sudo apt install sddm -y
 sudo systemctl enable sddm
 sudo systemctl set-default graphical.target
 sudo apt install kde-plasma-desktop -y
+sudo apt autoremove --purge konqueror -y
+sudo apt autoremove --purge kate -y
+sudo apt autoremove --purge kwalletmanager -y 
+sudo apt autoremove --purge kfind -y
+}
+
+case $ID in
+  debian) # Matches "debian"
+    echo "This machine is running Debian";
+    debianinstall; 
+    ;; 
+  kali) # Matches "arch"
+    echo "This machine is running Kali Linux";
+    sleep 5;	
+    ;;
+  zorin) # Matches "zorin"
+    echo "This machine is running Zorin";
+    sleep 5;
+    ;;	 
+  *) # This is the default case
+    echo "This machine is running $ID";
+    sleep 10 
+    ;;
+esac # ends case statement
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install nala -y
@@ -38,13 +68,9 @@ sudo snap install okular
 sudo snap install notepad-plus-plus
 sudo snap install apple-music-for-linux
 sudo snap install audible-for-linux
-sudo apt autoremove --purge konqueror -y
-sudo apt autoremove --purge kate -y
-sudo apt autoremove --purge kwalletmanager -y 
-sudo apt autoremove --purge kfind -y
 
-echo “Preparing cryptographic signatures for advance setup. Proceed?”
-echo "Installation complete. Media will now self delete. Have a good day."
-
-sleep 10
-#sudo shutdown -r now
+echo "Installation complete, device will now restart."
+#echo “Preparing cryptographic signatures for advance setup. Proceed?”
+#echo "Installation complete. Media will now self delete. Have a good day."
+#sleep 10
+sudo shutdown -r now
