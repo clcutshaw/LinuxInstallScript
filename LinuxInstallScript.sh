eae4b2f5e592794d6ff0545be23430c6bd45b7ce
clcutshaw <99746483+clcutshaw@users.noreply.github.com>
2025-05-05 15:50:07 -0600

    Update LinuxInstallScript.sh

--- LinuxInstallScript.sh
+++ LinuxInstallScript.sh
@@ -3,7 +3,7 @@
 #To be run after adding machine local user to sudo group
 echo "Welcome User. Beginning installation of your Linux Configuration"
 
-interaciveyn() { #Function for y/n uder interact
+interaciveyn() { #Function for y/n userr interact
 while true; do
 
 read -p "Do you want to proceed?" yn
@@ -21,7 +21,7 @@ esac
 done
 }
 
-interactiveyn
+interaciveyn
 
 sudo apt update
 sudo apt upgrade -y
